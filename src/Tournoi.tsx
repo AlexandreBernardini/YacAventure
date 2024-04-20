47b596c7a0a54c118e6e77339a797f16dfcdde9d
AlexandreBernardini <bernardini.alexandre2003@gmail.com>
2024-04-20 12:27:43 +0200

    mis en place correcte

--- src/Tournoi.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Text, View } from 'react-native';
-export default function Tournoi() {
-    return (
-        <View>
-            <Text>Tournoi</Text>
-        </View>
-    );
-}
